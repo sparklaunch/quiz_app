06d86a153ec932790a1acd675100ea0b85532263
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2024-08-10 11:07:16 +0900

    Update

--- lib/models/quiz_question.dart
+++ lib/models/quiz_question.dart
@@ -2,4 +2,9 @@ class QuizQuestion {
   final String text;
   final List<String> answers;
   const QuizQuestion(this.text, this.answers);
+  List<String> getShuffledAnswers() {
+    final List<String> shuffledList = List.of(answers);
+    shuffledList.shuffle();
+    return shuffledList;
+  }
 }
--- lib/screens/questions_screen.dart
+++ lib/screens/questions_screen.dart
@@ -15,22 +15,27 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
     final QuizQuestion currentQuestion = questions[0];
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            currentQuestion.text,
-            style: const TextStyle(
-              color: Colors.white,
+      child: Container(
+        margin: const EdgeInsets.all(30),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
             ),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          ...currentQuestion.answers.map((answer) {
-            return AnswerButton(answer, onTap: () => {});
-          }),
-        ],
+            const SizedBox(
+              height: 30,
+            ),
+            ...currentQuestion.getShuffledAnswers().map((answer) {
+              return AnswerButton(answer, onTap: () => {});
+            }),
+          ],
+        ),
       ),
     );
   }
